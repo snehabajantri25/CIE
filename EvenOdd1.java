b9064e76e871c5b5b0ac81237657fd0c22e88dd8
Snehabajantri25 <snehabajantri051@gmail.com>
2025-04-09 10:53:41 +0530

    evenodd

---
+++ EvenOdd1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenOdd1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even number.");
+        } else {
+            System.out.println(number + " is odd number.");
+        }
+
+        scanner.close();
+    }
+}
